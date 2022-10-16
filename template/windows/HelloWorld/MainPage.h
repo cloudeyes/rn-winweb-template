f8f9534086aaf3db84820ed933ec6512d91800d6
Joseph Kim <cloudeyes@gmail.com>
2022-10-16 23:33:22 +0900

    initial commit

---
+++ template/windows/HelloWorld/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::HelloWorld::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::HelloWorld::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
